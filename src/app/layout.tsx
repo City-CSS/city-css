9d105c310ff1446be6433174bf34b814f6fb0e54
Filippo Vicini <filippo.vicini2@gmail.com>
2024-09-21 12:18:51 +0100

    Various UI changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Computer Science Society",
+  title: "CSS",
   description: "City Unviersity London",
 };
 
