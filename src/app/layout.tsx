ca8ee6629ac93c84d81a40f042b3a3bf0e08df79
OlegPaska <olegpaska.2005@gmail.com>
2024-12-09 13:53:30 +0000

    tets

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
-
+// test
 export const metadata: Metadata = {
   title: "CSS",
   description: "City Unviersity London",
