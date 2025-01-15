f26f9fa8301c685eeac231f0931284f3ec2f2d03
Danylo Piatyhorets <Danylo.Piatyhorets@city.ac.uk>
2025-01-15 17:22:53 +0000

    done our program

---
+++ src/app/about-us/components/EventType.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function EventType (
+    {
+        icon: Icon,
+        iconSize = 100,
+        title,
+        description
+    }
+) {
+    return (
+        <div
+            className="rounded-2xl border border-gray-200 p-8 justify-items-center
+                       hover:border-red-500 hover:shadow-lg transition-all duration-300"
+        >
+            {Icon && <Icon size={iconSize} className="text-gray-600" />}
+            <h3 className="mt-4 font-semibold text-gray-900 text-xl">
+                {title}
+            </h3>
+            <p className="mt-2 text-gray-700">
+                {description}
+            </p>
+        </div>
+    );
+}
