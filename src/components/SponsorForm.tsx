aa394d4962c3fbc1f64eefab2b0f6b99869fcf5c
Danylo Piatyhorets <Danylo.Piatyhorets@city.ac.uk>
2025-01-17 12:30:17 +0000

    changed title of SponsorForm to 'collaborate'

--- src/components/SponsorForm.tsx
+++ src/components/SponsorForm.tsx
@@ -14,7 +14,7 @@ export default function Banner() {
       <div className="relative mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="mx-auto max-w-lg text-center">
           <h2 className="font-display text-4xl tracking-tight text-white font-semibold sm:text-5xl">
-            Want to sponsor?
+            Want to collaborate?
           </h2>
           <p className="mt-4 text-lg tracking-tight text-white">
             Power a community of tech enthusiasts by becoming a sponsor. 
