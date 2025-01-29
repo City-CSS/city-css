e1d8ca6833cce18c7fb6b3ea7ad9ad6f464682cc
Danylo Piatyhorets <Danylo.Piatyhorets@city.ac.uk>
2025-01-29 00:08:58 +0000

    refactored Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,47 @@
 import Image from "next/image";
 import Cube from "../assets/cube.png";
+import React from "react";
+import { FaGraduationCap, FaLinkedinIn, FaInstagram, FaWhatsapp, FaDiscord, FaEnvelope } from "react-icons/fa";
+import { IconType } from "react-icons";
+
+const linkPropsSociety = [
+  {
+    name: "Student Union",
+    link: "https://www.csgsu.co.uk/community/societies/society/css/",
+    icon: FaGraduationCap
+  },
+  {
+    name: "LinkedIn",
+    link: "https://www.linkedin.com/company/css-city/",
+    icon: FaLinkedinIn,
+  },
+  {
+    name: "Instagram",
+    link: "https://www.instagram.com/css_city/",
+    icon: FaInstagram
+  }
+]
+const linkPropsGetInTouch = [
+  {
+    name: "WhatsApp",
+    link: "https://chat.whatsapp.com/JENi7yzZA5T4wFDWIEqb03",
+    icon: FaWhatsapp
+  },
+  {
+    name: "Discord",
+    link: "https://discord.com/invite/MqzTbKsu",
+    icon: FaDiscord
+  },
+  {
+    name: "Email",
+    link: "mailto: citycss@outlook.com",
+    icon: FaEnvelope
+  }
+]
+
 export default function Footer() {
   return (
-    <div className="relative border-t border-gray-100 lg:pt-32 pb-8 dark:border-gray-800 lg:px-0 md:px-0 px-10">
+    <div className="relative border-t border-gray-100 pt-6 lg:pt-16 pb-8 dark:border-gray-800 lg:px-0 md:px-0 px-10">
       <div
         className="absolute inset-x-0 inset-y-0 -z-10 transform-gpu overflow-hidden blur-3xl"
         aria-hidden="true"
@@ -20,128 +59,24 @@ export default function Footer() {
           <div className="col-span-8 md:col-span-2 lg:col-span-3">
             <div className="flex h-full items-center justify-between gap-6 border-b border-white py-6 dark:border-gray-800 md:flex-col md:items-start md:justify-between md:space-y-6 md:border-none md:py-0">
               <div>
-                <Image src={Cube} alt="City University Logo" className="w-16" />
+                <Image src={Cube} alt="City University Logo" className="w-24" />
                 <a href="/" className="mt-2 inline-block text-sm">
                   Designed with Love
-                </a>
-              </div>
-              <div className="flex gap-6">
-                <a
-                  href="https://www.instagram.com/css_city/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  aria-label="instagram"
-                  className="hover:text-primary dark:hover:text-primaryLight"
-                >
-                  <span className="sr-only">Instagram</span>
-                  <svg
-                    className="w-6 h-6"
-                    aria-hidden="true"
-                    fill="currentColor"
-                    viewBox="0 0 24 24"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
-                    ></path>
-                  </svg>
-                </a>
-
-                <a
-                  href="/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  aria-label="linkedin"
-                  className="hover:text-primary dark:hover:text-primaryLight"
-                >
-                  <span className="sr-only">Linkedin</span>
-                  <svg
-                    fill="currentColor"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
-                  </svg>
                 </a>
               </div>
             </div>
           </div>
 
           <div className="col-span-8 md:col-span-6 lg:col-span-5">
-            <div className="grid grid-cols-2 gap-6 pb-16 sm:grid-cols-3 md:pl-16">
-              <div>
-                <h2 className="text-base font-medium text-gray-800 dark:text-gray-200">
-                  Society
-                </h2>
-                <ul className="mt-4 list-inside space-y-4">
-                  <li>
-                    <a
-                      href="https://www.csgsu.co.uk/community/societies/society/css/"
-                      className="text-sm duration-100 hover:text-primary dark:hover:text-white"
-                    >
-                      Students Union
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      className="text-sm duration-100 hover:text-primary dark:hover:text-white"
-                    >
-                      About Us
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      className="text-sm duration-100 hover:text-primary dark:hover:text-white"
-                    >
-                      Contacts
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="https://www.csgsu.co.uk/community/societies/society/css/"
-                      className="text-sm duration-100 hover:text-primary dark:hover:text-white"
-                    >
-                      Join{" "}
-                    </a>
-                  </li>
-                </ul>
-              </div>
-
-              <div>
-                <h2 className="text-base font-medium text-gray-800 dark:text-gray-200">
-                  Socials
-                </h2>
-                <ul className="mt-4 list-inside space-y-4">
-                  <li>
-                    <a
-                      href="https://discord.gg/MqzTbKsu"
-                      className="text-sm duration-100 hover:text-primary dark:hover:text-white"
-                    >
-                      Discord
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="https://chat.whatsapp.com/JENi7yzZA5T4wFDWIEqb03"
-                      className="text-sm duration-100 hover:text-primary dark:hover:text-white"
-                    >
-                      Whatsapp{" "}
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="https://www.instagram.com/css_city/"
-                      className="text-sm duration-100 hover:text-primary dark:hover:text-white"
-                    >
-                      Instagram{" "}
-                    </a>
-                  </li>
-                </ul>
-              </div>
+            <div className="grid grid-cols-2 gap-6 pb-8 sm:grid-cols-3 md:pl-12">
+              <Column
+                  title={"Society"}
+                  props={linkPropsSociety}
+              />
+              <Column
+                  title={"Get In Touch"}
+                  props={linkPropsGetInTouch}
+              />
             </div>
           </div>
         </div>
@@ -163,5 +98,43 @@ export default function Footer() {
         </div>
       </div>
     </div>
+  );
+}
+
+// Used to display columns of links in the Footer
+function Column (
+    {
+      title,
+      props
+    }: {
+      title : string
+      props: { name: string; link: string; icon: IconType }[]
+    }
+) {
+  return (
+      <div>
+        {/* Title */}
+        <h2 className="text-base font-medium text-gray-800 dark:text-gray-200">
+          {title}
+        </h2>
+
+        {/* List of Items */}
+        <div className="mt-4 space-y-4">
+          {props.map((prop, index) => (
+              <div key={index}>
+                <a
+                    target="_blank"
+                    href={prop.link}
+                    className="text-base duration-100 hover:text-primary dark:hover:text-white inline-flex items-center gap-2"
+                >
+                  {/* Icon */}
+                  <prop.icon className="text-gray-600 hover:text-gray-900" size={20} />
+                  {/* Name */}
+                  {prop.name}
+                </a>
+              </div>
+          ))}
+        </div>
+      </div>
   );
 }
